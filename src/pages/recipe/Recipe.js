499485635647e48fe52ba0747fe5981797928d25
mcorcias <mosescorcias@gmail.com>
2022-10-11 22:36:11 +0300

    11.10.22

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,15 +1,17 @@
 import './Recipe.css'
-import { useParams } from 'react-router-dom'
+import { useParams} from 'react-router-dom'
 import {useFetch} from '../../hooks/useFetch'
 
 
+
 export default function Recipe() {
+
+
   const {id} = useParams()
 
   const {data: recipe ,error, isPending} = useFetch(`http://localhost:3000/recipes/${id}`)
 
   return (
-
     <div className='recipe'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
