0e1c59895911a3cbf395030def333d4f97b0b5ad
mcorcias <mosescorcias@gmail.com>
2022-10-16 03:21:09 +0300

    16.10.22

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import SearchBar from './SearchBar'
 
 import './NavBar.css'
 import React from 'react'
@@ -8,6 +9,7 @@ export default function NavBar() {
     <div className='navbar'>
         <nav>
             <Link to='/' className='brand'><h1>Cooking Ninja</h1></Link>
+            <SearchBar />
             <Link to='/create'><h1>Create Recipe</h1></Link>
         </nav>
     </div>
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,15 +1,51 @@
-import React, { useState } from 'react'
+import React, {  useEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import {useFetch} from '../../hooks/useFetch'
 import './Create.css'
 
 export default function Create() {
   const [title,setTitle] = useState('')
   const [method,setMethod] = useState('')
-  const [cookingTime,setCookingTime] = useState()
+  const [cookingTime,setCookingTime] = useState('')
+  const [newIngredient,setNewIngredient] = useState('')
+  const [ingredients,setIngredients] = useState([])
+  const ingredientInput = useRef(null)
+  const {push} = useHistory()
+
+  
+  const {postData,data,error} = useFetch('http://localhost:3000/recipes','POST')
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('title, method, cookingTime')
+    console.log('check')
+    console.log(title, method, cookingTime, ingredients)
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + 'minutes',
+    })
   }
+
+  const handleAdd = () => {
+    const ing = newIngredient.trim()
+    if(ing && !ingredients.includes(ing)){
+      setIngredients((prev) => [...prev,ing])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
+  }
+  
+  useEffect(()=>{
+    if(data){
+      push('/')
+    }
+    if(error){
+      console.log('error');
+    }
+  },[data,error,push])
+
 
   return (
     <div className='create'>
@@ -21,12 +57,23 @@ export default function Create() {
           <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required />
         </label>
         <label>
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+            <input type="text" ref={ingredientInput} onChange={(e)=>setNewIngredient(e.target.value)} value={newIngredient} />
+            <button type='button' className='btn' onClick={handleAdd}>add</button>
+          </div>
+        </label>
+        <p>Current Ingredients: {ingredients.map(ing => (
+          <em key={ing}>{ing}, </em>
+        ))}
+        </p>
+        <label>
           <span>Recipe Method:</span>
           <textarea onChange={(e) => setMethod(e.target.value)} value={method} required/>
         </label>
         <label>
           <span>Cookie time (minutes):</span>
-          <input type="number" onChange={(e) => setCookingTime(e.value)} value={cookingTime} required />
+          <input type="number" onChange={(e) => setCookingTime(e.target.value)} value={cookingTime} required />
         </label>
 
         <button className='btn'>Submit</button>
